938fe1d71e87381a3ab197160c90a70fa6c99e73
GOCrazy <83633399+gocrazygh@users.noreply.github.com>
2022-05-04 12:06:40 +0800

    Update cipher.go
    Better encryption

--- cipher.go
+++ cipher.go
@@ -3,60 +3,67 @@ package cipher
 import (
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/md5"
 	"crypto/rand"
-	"encoding/hex"
 	"io"
-	"io/ioutil"
-	"os"
+
+	"golang.org/x/crypto/sha3"
 )
 
-func CreateHash(key string) string {
-	hasher := md5.New()
-	hasher.Write([]byte(key))
-	return hex.EncodeToString(hasher.Sum(nil))
+func Encrypt(message []byte, passPhrase string) ([]byte, error) {
+	key := Hash(passPhrase)
+	return EncryptAES256(key, message)
 }
 
-func Encrypt(data []byte, passphrase string) []byte {
-	block, _ := aes.NewCipher([]byte(CreateHash(passphrase)))
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err.Error())
-	}
-	nonce := make([]byte, gcm.NonceSize())
-	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
-	}
-	ciphertext := gcm.Seal(nonce, nonce, data, nil)
-	return ciphertext
+func Decrypt(message []byte, passPhrase string) ([]byte, error) {
+	key := Hash(passPhrase)
+	return DecryptAES256(key, message)
 }
 
-func Decrypt(data []byte, passphrase string) []byte {
-	key := []byte(CreateHash(passphrase))
+func EncryptAES256(key []byte, plaintext []byte) (encmess []byte, err error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err.Error())
+		return
 	}
-	gcm, err := cipher.NewGCM(block)
+
+	aesGCM, err := cipher.NewGCM(block)
 	if err != nil {
-		panic(err.Error())
+		return
 	}
-	nonceSize := gcm.NonceSize()
-	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
-	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		panic(err.Error())
+
+	nonce := make([]byte, aesGCM.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return
 	}
-	return plaintext
+
+	ciphertext := aesGCM.Seal(nonce, nonce, plaintext, nil)
+	encmess = ciphertext
+	return
 }
 
-func EncryptFile(filename string, data []byte, passphrase string) {
-	f, _ := os.Create(filename)
-	defer f.Close()
-	f.Write(Encrypt(data, passphrase))
+func DecryptAES256(key []byte, securemess []byte) (decodedmess []byte, err error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return
+	}
+
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		return
+	}
+
+	nonceSize := aesGCM.NonceSize()
+
+	nonce, ciphertext := securemess[:nonceSize], securemess[nonceSize:]
+
+	plaintext, err := aesGCM.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return
+	}
+	decodedmess = plaintext
+	return
 }
 
-func DecryptFile(filename string, passphrase string) []byte {
-	data, _ := ioutil.ReadFile(filename)
-	return Decrypt(data, passphrase)
+func Hash(s string) []byte {
+	h := sha3.Sum256([]byte(s))
+	return h[:]
 }
