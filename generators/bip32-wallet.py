8015537e53581376bfea9c2cb371c41aafa1b785
GOCrazy7575 <83633399+gocrazygh@users.noreply.github.com>
2022-06-07 12:53:30 +0800

    Added main function

--- generators/bip32-wallet.py
+++ generators/bip32-wallet.py
@@ -1,22 +1,29 @@
 from mnemonic import Mnemonic
 import bip32utils
-#Generate new seed phrase
-mnemon = Mnemonic("english")
-words = mnemon.generate(256)
-mnemon.check(words)
-seed = mnemon.to_seed(words)
 
-RootKey = bip32utils.BIP32Key.fromEntropy(seed)
-#Create new child key
-ChildKey = RootKey.ChildKey(0).ChildKey(0)
-#Create new public address
-PublicAddress = ChildKey.Address()
-#Create new public key
-PublicKey = ChildKey.PublicKey().hex()
-#Create new private key
-PrivateKey = ChildKey.WalletImportFormat()
 
-print("Generated BIP32 Wallet: ")
-print("Public Address: " + PublicAddress)
-print("Public Key: " + PublicKey)
-print("Private Key: " + PrivateKey)
+def main():
+    # Generate new seed phrase
+    mnemon = Mnemonic("english")
+    words = mnemon.generate(256)
+    mnemon.check(words)
+    seed = mnemon.to_seed(words)
+
+    RootKey = bip32utils.BIP32Key.fromEntropy(seed)
+    # Create new child key
+    ChildKey = RootKey.ChildKey(0).ChildKey(0)
+    # Create new public address
+    PublicAddress = ChildKey.Address()
+    # Create new public key
+    PublicKey = ChildKey.PublicKey().hex()
+    # Create new private key
+    PrivateKey = ChildKey.WalletImportFormat()
+
+    print("Generated BIP32 Wallet: ")
+    print("Public Address: " + PublicAddress)
+    print("Public Key: " + PublicKey)
+    print("Private Key: " + PrivateKey)
+
+
+if __name__ == "__main__":
+    main()
