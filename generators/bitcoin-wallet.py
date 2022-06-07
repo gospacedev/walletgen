71d66527fe18f4d93f757191cffb950544dc81ae
GOCrazy7575 <83633399+gocrazygh@users.noreply.github.com>
2022-06-07 12:52:04 +0800

    Update bitcoin-wallet.py

--- generators/bitcoin-wallet.py
+++ generators/bitcoin-wallet.py
@@ -1,10 +1,17 @@
 from bitcoin import *
-#Generate new private key
-PrivateKey = random_key()
-#Generate new public key from private key
-PublicKey = privtopub(PrivateKey)
-#Create new address from public key
-addr = pubtoaddr(PublicKey)
 
-print("My Public Address: " + addr)
-print("My Private Address: " + PrivateKey)
+
+def main():
+    # Generate new private key
+    PrivateKey = random_key()
+    # Generate new public key from private key
+    PublicKey = privtopub(PrivateKey)
+    # Create new address from public key
+    addr = pubtoaddr(PublicKey)
+
+    print("My Public Address: " + addr)
+    print("My Private Address: " + PrivateKey)
+
+
+if __name__ == "__main__":
+    main()
