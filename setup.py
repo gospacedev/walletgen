a49e7797be2f7afe98ff21c6c57bb002c2b33049
Grantley Cullar <grantcullar@gmail.com>
2022-11-25 18:44:01 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='walletgen'
+    version='1.0',
+    author='Grantley Cullar',
+    description='A library of simple bitcoin wallet generators',
+    url='https://github.com/gospacedev/walletgen',
+    install_requires=[
+        'bitcoin',
+        'Mnemonic',
+        'bip32utils'
+    ],
+    }
+)
